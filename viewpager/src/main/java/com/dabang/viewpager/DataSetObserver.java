f9621c4005d319a71be169b55fc5eefc760ab4db
liaoyuhuan <liaoyuhuan@1dabang.cn>
2017-07-28 11:03:04 +0800

    拎出viewpager源码，用于定Z制特殊需求

---
+++ viewpager/src/main/java/com/dabang/viewpager/DataSetObserver.java
@@ -0,0 +1,26 @@
+package com.dabang.viewpager;
+
+import android.database.Cursor;
+
+/**
+ * Created by Jane on 2017/7/28.
+ */
+
+public abstract class DataSetObserver {
+    /**
+     * This method is called when the entire data set has changed,
+     * most likely through a call to {@link Cursor#requery()} on a {@link Cursor}.
+     */
+    public void onChanged() {
+        // Do nothing
+    }
+
+    /**
+     * This method is called when the entire data becomes invalid,
+     * most likely through a call to {@link Cursor#deactivate()} or {@link Cursor#close()} on a
+     * {@link Cursor}.
+     */
+    public void onInvalidated() {
+        // Do nothing
+    }
+}
